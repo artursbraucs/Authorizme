22f96b22189959c5b3d215fa83d2aafa10c1ba6e
Arturs Braucs <arturs@creo.mobi>
2012-02-27 15:23:05 +0200

    Files restruct

--- lib/authorizme/engine.rb
+++ lib/authorizme/engine.rb
@@ -1,11 +1,5 @@
 module Authorizme
   class Engine < Rails::Engine
 
-    initialize "authorizme.load_app_instance_data" do |app|
-      Authorizme.setup do |config|
-        config.app_root = app.root
-      end
-    end
-
   end
 end
